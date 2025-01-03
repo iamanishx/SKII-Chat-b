3eb2449c1ea32637af93c1df8be140e1cd9ae44b
iamanishx <manishbiswal754@gmail.com>
2025-01-03 20:32:36 +0530

    cors

--- src/authentication.js
+++ src/authentication.js
@@ -66,7 +66,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://skii-chat.up.railway.app/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
