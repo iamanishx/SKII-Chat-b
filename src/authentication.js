27d97de3444108f23fe45aae01da79b4ef8079ae
iamanishx <manishbiswal754@gmail.com>
2025-01-11 22:42:10 +0530

    nt

--- src/authentication.js
+++ src/authentication.js
@@ -24,7 +24,7 @@ const User = mongoose.model("User", userSchema);
 
 // Create Express App
 const app = express();
-app.set("trust proxy", 1);
+app.set("trust proxy", true);
 
 // Middleware
 // Define CORS options
