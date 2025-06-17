b69e862146a0699daec9ef01d0e5ff9bf0d60db3
iamanishx <manishbiswal754@gmail.com>
2025-06-17 15:34:14 +0530

    fix

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const cors = require("cors");
 
 const app = express();   
 
-
-app.use(cors(corsOptions));
-app.options('*', cors()); // Handle preflight
+\
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
